905237bf0afea21b22d12c1653b7d5f0bdbe3ffd
JaKsHaZbi <joker_hazbi@mail.ru>
2022-02-24 19:12:32 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let myConf = bracketsConfig.map(item => item.join(''))
+
+  for(let j = -1 ; j < myConf.length; j++){
+    for (let i = 0; i < myConf.length; i++) {
+         if (str.includes(myConf[i])) {
+            str = str.replace(myConf[i], '')
+            i-- 
+        }
+    }
+  }
+
+ return !str.length ? true : false
 }
